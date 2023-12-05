ec5143b22c26cfcd56e0a88bdde8c21d41f0afb6
BiBCast <pietrocast03@gmail.com>
2023-12-05 09:36:02 +0100

    init: scaffold project

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/chat", (req, res) => {
+  res.send("chat message");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
