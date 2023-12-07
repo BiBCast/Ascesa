68b524d508d123790db4f24318cb96f17442cd46
BiBCast <pietrocast03@gmail.com>
2023-12-07 17:01:45 +0100

    feature: basic graphql

--- index.ts
+++ index.ts
@@ -4,11 +4,48 @@ import http from "http";
 import path from "path";
 import { Server, } from "socket.io";
 
+import { graphqlHTTP } from "express-graphql"
+import { buildSchema } from "graphql";
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+
+
 const server = http.createServer(app);
 
 const io = new Server(server);
 app.get("/", (req: Request, res:Response) => {
-  res.sendFile(path.join(__dirname,"../index.html"));
+  
+  res.sendFile(path.join(__dirname,"../chat.html"));
 });
 
 io.on("connection", (socket) => {
