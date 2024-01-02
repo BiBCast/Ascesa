fe504abd324ba6e38073494fefcac46add01baee
BiBCast <pietrocast03@gmail.com>
2024-01-02 17:09:16 +0100

    feature: refactoring

--- client/src/components/InputBar/InputBar.tsx
+++ client/src/components/InputBar/InputBar.tsx
@@ -1,12 +1,22 @@
-import { KeyboardEvent, useState } from "react";
+import { Dispatch, KeyboardEvent, useState } from "react";
 import "./index.css";
 import { io } from "socket.io-client";
+import { ChatUser } from "../../App";
 const socket = io("http://localhost:3000/");
-export default function InputBar() {
+export default function InputBar({
+  setChatUsers,
+}: {
+  setChatUsers: Dispatch<ChatUser[]>;
+}) {
   const [input, setInput] = useState("");
   function handleInput(e: KeyboardEvent<HTMLInputElement>) {
     if (e.key === "Enter") {
       if (input === "") return;
+      console.log(input);
+      //TODO implement user
+      setChatUsers((prev: ChatUser[]) => {
+        [...prev, { user: "pietro", message: input } as ChatUser];
+      });
       socket.emit("chat message", "pietro;" + input);
     }
   }
--- client/src/components/Messages/Messages.tsx
+++ client/src/components/Messages/Messages.tsx
@@ -6,10 +6,13 @@ export default function Messages({ ChatUsers }: { ChatUsers: ChatUser[] }) {
     <>
       <div className="Messages">
         {ChatUsers?.map((user: ChatUser, index: number) => (
-          <ul key={index}>
-            <li>User: {user.user}</li>
-            <li>Message: {user.message}</li>
-          </ul>
+          <>
+            <ul key={index}>
+              <li>User: {user.user}</li>
+              <li>Message: {user.message}</li>
+            </ul>
+            <div>++++++++++++++++++++++</div>
+          </>
         ))}
       </div>
       <InputBar />
