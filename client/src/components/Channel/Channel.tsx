c014724bf3c5bdfbbf8cdaf6e1826595663061c6
BiBCast <pietrocast03@gmail.com>
2024-01-30 16:57:30 +0100

    test

---
+++ client/src/components/Channel/Channel.tsx
@@ -0,0 +1,13 @@
+export function Channel() {
+  return (
+    <>
+      <div className="cn">
+        <div>icon</div>
+        <div className="cn__text">
+          <div>title</div>
+          <div>last message</div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/Channels/Channels.tsx
@@ -0,0 +1,11 @@
+import { Channel } from "../Channel/Channel";
+import "./index.css";
+export function Channels() {
+  return (
+    <>
+      <div className="cns">
+        <Channel />
+      </div>
+    </>
+  );
+}
