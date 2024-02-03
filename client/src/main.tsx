b83a735987362486c953bf999f8f48d41f35d5f8
BiBCast <pietrocast03@gmail.com>
2024-02-03 15:41:33 +0100

    feature: server url for mobile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql/",
+  uri: "http://192.168.1.11:3000/graphql/",
   cache: new InMemoryCache(),
 });
 ReactDOM.createRoot(document.getElementById("root")!).render(
